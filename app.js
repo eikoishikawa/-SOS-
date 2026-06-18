0eff111a62ad412c57e6570ddf4601f042bcce26
eikoishikawa <eiko.ishikawa@constantiniye.com>
2026-06-18 17:56:30 +0900

    テスト貼り付け
    Added escapeHtml function to sanitize text input.

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+function escapeHtml(text) {
+  return String(text)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+// 避難所・給水所データを読み込む
+fetch("data/shelters.geojson")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    L.geoJSON(data, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 8
+        });
+      },
+      onEachFeature: function (feature, layer) {
+        const name = feature.properties.name || "名称未設定";
+        const type = feature.properties.type || "施設";
+        const memo = feature.properties.memo || "";
 
+        layer.bindPopup(`
+          <strong>${name}</strong><br>
+          種別：${type}<br>
+          ${memo}
+        `);
+      }
+    }).addTo(map);
+  })
+  .catch(function (error) {
+    console.error("GeoJSONを読み込めませんでした。", error);
+  });
